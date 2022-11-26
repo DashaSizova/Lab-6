2c5f4338de72565f11dd5f09d3375b2dc178527c
DashaSizova <118754652+DashaSizova@users.noreply.github.com>
2022-11-26 17:07:22 +0200

    Editorial

--- Код.cpp
+++ Код.cpp
@@ -18,3 +18,21 @@ int main()
     cin >> m;
     float rez1;
     int sum = 0;
+	    for (int i = 1; i <= 10; i++)
+    {
+        sum = sum + sin(k * x);
+    }
+    rez1 = (sum + 2 * x) / ((x * x) + 5);
+
+    float rez2;
+    int pr = 1;
+    for (int i = 2; i <= 10; i++)
+    {
+        pr = pr + (log(m * rez1));
+    }
+    rez2 = (3 * sin(rez1)) + pr;
+    cout << "Y=" << rez1 << endl;
+    cout << "Z=" << rez2 << endl;
+    system("pause");
+    return 0;
+}
