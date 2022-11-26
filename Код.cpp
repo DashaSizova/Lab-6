0b6fc3d45b8b961baa12ee557126123e7140d283
DashaSizova <118754652+DashaSizova@users.noreply.github.com>
2022-11-26 17:04:35 +0200

    New Code

---
+++ Код.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+#include <iomanip> 
+#include "math.h" 
+#include "windows.h"
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int k, x, m;
+    cout << "Введіть k =";
+    cin >> k;
+    cout << "Введіть x =";
+    cin >> x;
+    cout << "Введіть m =";
+    cin >> m;
+    float rez1;
+    int sum = 0;
